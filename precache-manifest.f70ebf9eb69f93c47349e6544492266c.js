444d1ac801a07ae795c1e43edd416f86148bf97f
Admin <xndqhacj@yandex.ru>
2020-09-16 11:32:12 +0200

    change homepage url

---
+++ precache-manifest.f70ebf9eb69f93c47349e6544492266c.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8fa92d58f941b265e45c5f10eee9b4",
+    "url": "/CDG_React/index.html"
+  },
+  {
+    "revision": "dc1fddf0602bbe97366c",
+    "url": "/CDG_React/static/css/2.c020e8f2.chunk.css"
+  },
+  {
+    "revision": "693e65380ab5397d1f08",
+    "url": "/CDG_React/static/css/main.b1030259.chunk.css"
+  },
+  {
+    "revision": "dc1fddf0602bbe97366c",
+    "url": "/CDG_React/static/js/2.896fb794.chunk.js"
+  },
+  {
+    "revision": "4fa2a7d361f8044c67c89394f3737882",
+    "url": "/CDG_React/static/js/2.896fb794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693e65380ab5397d1f08",
+    "url": "/CDG_React/static/js/main.049d2bbd.chunk.js"
+  },
+  {
+    "revision": "76468d91a3ac2f28d0ad",
+    "url": "/CDG_React/static/js/runtime-main.e68b9429.js"
+  },
+  {
+    "revision": "168fb5a467966bac2c4d60e06a836454",
+    "url": "/CDG_React/static/media/CalendarDayStatusDanger.168fb5a4.svg"
+  },
+  {
+    "revision": "b4118d290062db0e93ee2d9951434ad1",
+    "url": "/CDG_React/static/media/CalendarDayStatusOK.b4118d29.svg"
+  },
+  {
+    "revision": "ccaa2bfb69d8383cdf18d8efecf0bca2",
+    "url": "/CDG_React/static/media/CalendarDayStatusWarning.ccaa2bfb.svg"
+  },
+  {
+    "revision": "a07d91313a86fff4635d8126f7f76b3c",
+    "url": "/CDG_React/static/media/Cyan.a07d9131.svg"
+  },
+  {
+    "revision": "47285204347acdb19f807084d9fa57db",
+    "url": "/CDG_React/static/media/Orange.47285204.svg"
+  },
+  {
+    "revision": "f1ff4eee46d1d958bf2c154ad677a860",
+    "url": "/CDG_React/static/media/Purple.f1ff4eee.svg"
+  },
+  {
+    "revision": "9feb0110b6dff9ee2b9ebd17f7a1aee6",
+    "url": "/CDG_React/static/media/Roboto-Regular.9feb0110.woff2"
+  },
+  {
+    "revision": "8e7efc77f9acec892eccd324e35ab588",
+    "url": "/CDG_React/static/media/TT-pause-button.8e7efc77.svg"
+  },
+  {
+    "revision": "3d2bae0a2fb4375e250fbd5c7e03943d",
+    "url": "/CDG_React/static/media/TT-stop-button.3d2bae0a.svg"
+  },
+  {
+    "revision": "89696082234753ee0b96c6fd344353b1",
+    "url": "/CDG_React/static/media/active-issue-more-button.89696082.svg"
+  },
+  {
+    "revision": "0dc941bd70039d264febad7d7314b3cc",
+    "url": "/CDG_React/static/media/arrow-up.0dc941bd.svg"
+  },
+  {
+    "revision": "462c6cf416c7caf6c1cffd86412a66cb",
+    "url": "/CDG_React/static/media/avatar.462c6cf4.svg"
+  },
+  {
+    "revision": "daeb1a9d1f1a5473f187b8c4ab09098b",
+    "url": "/CDG_React/static/media/calendar.daeb1a9d.svg"
+  },
+  {
+    "revision": "daeb1a9d1f1a5473f187b8c4ab09098b",
+    "url": "/CDG_React/static/media/calendar_active.daeb1a9d.svg"
+  },
+  {
+    "revision": "aea2c3bfdbe55ce3d7a33b3a46b060f4",
+    "url": "/CDG_React/static/media/calendar_gray.aea2c3bf.svg"
+  },
+  {
+    "revision": "8d55a97670fa4f1bb45e115d38a27741",
+    "url": "/CDG_React/static/media/confirmButton.8d55a976.svg"
+  },
+  {
+    "revision": "49659e507dbe61bd91bdcd5d481e5821",
+    "url": "/CDG_React/static/media/danger_cp.49659e50.svg"
+  },
+  {
+    "revision": "9823975fa5eef853ef2c52acc9050ce1",
+    "url": "/CDG_React/static/media/declineButton.9823975f.svg"
+  },
+  {
+    "revision": "ffe979f7d0ef19050b1788dc5aa7cdfa",
+    "url": "/CDG_React/static/media/door.ffe979f7.svg"
+  },
+  {
+    "revision": "07db24c65c0907d57119b856e7e538ec",
+    "url": "/CDG_React/static/media/download.07db24c6.svg"
+  },
+  {
+    "revision": "ee54e2c244cce12be134faaa6ca6c2b8",
+    "url": "/CDG_React/static/media/new_worklog_button.ee54e2c2.svg"
+  },
+  {
+    "revision": "6b0a0400620c54abe367ea5487ac4a62",
+    "url": "/CDG_React/static/media/new_worklog_button_shadowed.6b0a0400.svg"
+  },
+  {
+    "revision": "08ca5ceee679fc8c316bcdde2ac27b6c",
+    "url": "/CDG_React/static/media/ok_cp.08ca5cee.svg"
+  },
+  {
+    "revision": "f9fec951617f9bffd07127f39b18e6f4",
+    "url": "/CDG_React/static/media/play-button.f9fec951.svg"
+  },
+  {
+    "revision": "07aaae672aa6bd633d5158f135f5be8a",
+    "url": "/CDG_React/static/media/second_icon.07aaae67.svg"
+  },
+  {
+    "revision": "ec52537ee4e6fc553fc659a1f01aef8f",
+    "url": "/CDG_React/static/media/separator-line.ec52537e.svg"
+  },
+  {
+    "revision": "6677f6c8c8d6230c0361b9ac3b64d44f",
+    "url": "/CDG_React/static/media/settings.6677f6c8.svg"
+  },
+  {
+    "revision": "1eb1b96f76c8da38135e69f2f8b98fb7",
+    "url": "/CDG_React/static/media/stop_button.1eb1b96f.svg"
+  },
+  {
+    "revision": "2e15c5b7a63e8cfe36b363e37bc4b1bb",
+    "url": "/CDG_React/static/media/stroke_horiz.2e15c5b7.svg"
+  },
+  {
+    "revision": "437b293842dd1949f53e8d0faf05f43b",
+    "url": "/CDG_React/static/media/stroke_vert.437b2938.svg"
+  },
+  {
+    "revision": "c2d1e025c85c0522d31ccf79d7e2a2f3",
+    "url": "/CDG_React/static/media/warning_cp.c2d1e025.svg"
+  }
+]);
